bc1f18987591a498a674121cf954a9b4f03afcbb
Bailiev Allanazar <a.baylyyew05@gmail.com>
2025-04-11 20:40:50 +0300

    Update router.py and main.py

--- main.py
+++ main.py
@@ -9,37 +9,5 @@ app = FastAPI()
 
 app.include_router(router)
 
-
-class User(BaseModel):
-    first_name: str
-    last_name: str
-    age: int
-
-
-user_table = [
-    User(first_name="user1", last_name="last_name1", age=2135),
-    User(first_name="user2", last_name="last_name1", age=55),
-    User(first_name="user3", last_name="lastname_666", age=20),
-]
-
-
-@app.get("/all", response_model=List[User])
-async def get_all_users():
-    return user_table
-
-# 2. Эндпоинт для создания нового пользователя
-@app.post("/create", response_model=User)
-async def create_user(user: User):
-    user_table.append(user)
-    return user
-
-# 3. Эндпоинт для поиска пользователя по имени
-@app.get("/get_by_name", response_model=List[User])
-async def get_user_by_name(first_name: str):
-    found_users = [u for u in user_table if u.first_name == first_name]
-    if not found_users:
-        raise HTTPException(status_code=404, detail="User not found")
-    return found_users
-
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True) 
--- router.py
+++ router.py
@@ -1,26 +1,37 @@
-from typing import Union
-from fastapi import APIRouter
+from typing import List
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 
 router = APIRouter()
 
+# Дочерний класс для создания пользователя
+class User(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
+# Имитация БД
+user_table = [
+    User(first_name="user1", last_name="last_name1", age=2135),
+    User(first_name="user2", last_name="last_name1", age=55),
+    User(first_name="user3", last_name="lastname_666", age=20),
+]
 
+# 1. Эндпоинт для вывода всех пользователей 
+@router.get("/all", response_model=List[User])
+async def get_all_users():
+    return user_table
 
-@router.get("/")
-def read_root():
-    return {"Hello": "World"}
+# 2. Эндпоинт для создания нового пользователя
+@router.post("/create", response_model=User)
+async def create_user(user: User):
+    user_table.append(user)
+    return user
 
-
-@router.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@router.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+# 3. Эндпоинт для поиска пользователя по имени
+@router.get("/get_by_name", response_model=List[User])
+async def get_user_by_name(first_name: str):
+    found_users = [u for u in user_table if u.first_name == first_name]
+    if not found_users:
+        raise HTTPException(status_code=404, detail="User not found")
+    return found_users
