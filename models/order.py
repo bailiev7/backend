c83c577819b86968d9ea560f28dd2ce8974bf361
Bailiev Allanazar <a.baylyyew05@gmail.com>
2025-04-20 18:51:02 +0300

    delete index=True

--- models/order.py
+++ models/order.py
@@ -4,7 +4,7 @@ from database import Base
 class Order(Base):
     __tablename__ = "orders"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("users.id"))
     name = Column(String)
     sum = Column(Integer)
