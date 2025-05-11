f9e0b7960b6cad4d6b04adca41b7870661381181
Bailiev Allanazar <a.baylyyew05@gmail.com>
2025-05-11 21:17:26 +0300

    added router set/role

--- domain/user/router.py
+++ domain/user/router.py
@@ -2,9 +2,23 @@ from fastapi import APIRouter, HTTPException
 from sqlalchemy import select
 from database import async_session_maker
 from models.user import User as DBUser
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
+from models.role_enum import RoleEnum
+from models.user_role import UserRole
 
 user_router = APIRouter(prefix="/user")
+
+
+class SetUserRoleRequest(BaseModel):
+    user_id: int
+    role: str
+
+    @field_validator("role")
+    def normalize_role(cls, value: str) -> str:
+        value = value.lower()
+        if value not in ("admin", "default_user"):
+            raise ValueError("role must be 'admin' or 'default_user'")
+        return value
 
 
 class UserCreate(BaseModel):
@@ -32,10 +46,30 @@ async def create_user(user: UserCreate):
 
 
 @user_router.get("/get_by_name", response_model=list[UserCreate])
-async def get_user_by_name(last_name: str):
-    stmt = select(DBUser).where(DBUser.last_name == last_name)
+async def get_user_by_name(first_name: str):
+    stmt = select(DBUser).where(DBUser.first_name == first_name)
     async with async_session_maker() as session:
         users = (await session.scalars(stmt)).all()
     if not users:
         raise HTTPException(status_code=404, detail="Пользователь не найден")
     return users
+
+
+@user_router.post("/set/role")
+async def set_role(data: SetUserRoleRequest):
+    stmt_user = select(DBUser).where(DBUser.id == data.user_id)
+    stmt_role = select(UserRole).where(
+        UserRole.role_name == data.role)
+    async with async_session_maker() as session:
+        user = (await session.scalars(stmt_user)).first()
+        if user is None:
+            raise HTTPException(
+                status_code=404, detail="Пользователь не найден")
+        role = (await session.scalars(stmt_role)).first()
+        if role is None:
+            raise HTTPException(status_code=404, detail="Роль не найдена")
+        user.role_id = role.id
+        await session.commit()
+        await session.refresh(user)
+
+    return {"message": f"Пользователю {user.first_name} назначена роль {data.role.value}"}
--- models/user_role.py
+++ models/user_role.py
@@ -1,11 +1,13 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, Enum
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
 from database import Base
+from models.role_enum import RoleEnum
 
 
 class UserRole(Base):
     __tablename__ = "user_role"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    role_name = Column(String, nullable=False)
+    role_name = Column(Enum(RoleEnum, name="roleenum",
+                       create_constraint=True), nullable=False, unique=True)
